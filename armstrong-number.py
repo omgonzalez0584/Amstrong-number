bda5064d240735d29ec3519f4a11645bda4957f9
Omar Gonzalez <om.gonzalez007@gmail.com>
2024-02-01 20:06:15 -0500

    Try - Except code

--- armstrong-number.py
+++ armstrong-number.py
@@ -8,15 +8,18 @@ def armstrong_numb(num):
         return True
     else:
         False
+try:
+    number = input("Enter a number: ")
+    result = armstrong_numb(number)
+    if result == True:
+        print(f"{number} is an armstrong number!")
+    else:
+        print(f"{number} is not an Armstrong number!")
+    
+except Exception as e:
+    print("Error, please enter a number!")
 
-number = input("Enter a number: ")
-result = armstrong_numb(number)
 
-
-if result == True:
-    print(f"{number} is an armstrong number!")
-else:
-    print(f"{number} is not an Armstrong number!")
 
     
 
